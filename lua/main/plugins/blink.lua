2be41e81dc052a0600f846cfaabccd4cea26a6b9
edamamet <adamtam2004@outlook.com>
2025-09-08 17:13:56 -0400

    update + blink->enter

--- lua/main/plugins/blink.lua
+++ lua/main/plugins/blink.lua
@@ -25,7 +25,7 @@ return {
         -- C-k: Toggle signature help (if signature.enabled = true)
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'default' },
+        keymap = { preset = 'enter' },
 
         signature = {
             enabled = true,
