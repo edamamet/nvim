a8917a6229a337f88d92d6f3b8aeace355d3a35f
edamamet <adamtam2004@outlook.com>
2026-01-12 21:00:24 -0500

    roslyn

--- lua/main/plugins/lsp.lua
+++ lua/main/plugins/lsp.lua
@@ -2,8 +2,19 @@ return {
     "mason-org/mason-lspconfig.nvim",
     opts = {},
     dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
+        { "mason-org/mason.nvim", opts = {
+            registries = {
+                "github:mason-org/mason-registry",
+                "github:Crashdummyy/mason-registry",
+            },
+        } },
         "neovim/nvim-lspconfig",
+        { 
+            "seblyng/roslyn.nvim",
+            ---@module 'roslyn.config'
+            ---@type RoslynNvimConfig
+            opts = {},
+        }
     },
     init = function()
         vim.keymap.set("n", "grh", "<CMD>lua vim.lsp.buf.hover()<CR>", { desc = "Show hover information" })
