6c65abc3a41c3f405f9ca78b05f34ec5d37c2984
oodum <adamtam2004@outlook.com>
2026-02-27 14:47:45 -0500

    lsp format keybind
    use "leader d f"

--- lua/main/core/keybinds.lua
+++ lua/main/core/keybinds.lua
@@ -4,5 +4,8 @@ keymap.set('n', 'j', 'gj', { desc = 'move down a visual line' })
 keymap.set('n', 'k', 'gk', { desc = 'move down a visual line' })
 keymap.set('v', 'j', 'gj', { desc = 'move selection down a visual line' })
 keymap.set('v', 'k', 'gk', { desc = 'move selection down a visual line' })
+
 keymap.set("n", "grD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { desc = 'go to declaration', noremap = true, silent = true })
 keymap.set("n", "grd", "<cmd>lua vim.lsp.buf.definition()<CR>", { desc = 'go to declaration', noremap = true, silent = true })
+
+keymap.set("n", "<Leader>df", "<cmd>lua vim.lsp.buf.format()<CR>", {desc = 'lsp format', noremap = true, silent = true})
