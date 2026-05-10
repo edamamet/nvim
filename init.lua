b374a9daf3acb6a05b54f17501a3260c4ee5e6ac
edamamet <adamtam2004@outlook.com>
2026-05-10 14:53:51 -0400

    tabstop=4

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("colorscheme catppuccin")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 
+vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set expandtab")
